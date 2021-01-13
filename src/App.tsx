5d73d5ceb67cd2c72bec5ccb31d1b07e93f113e6
redacted007 <77245032+redacted007@users.noreply.github.com>
2021-01-12 22:53:25 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -50,15 +50,116 @@ const ColorRush = styled.div`
   }
 `
 
+// .time {
+//   letter-spacing: 0.05em;
+//   font-size: 80px;
+//   padding: 5px 0;
+// }
+// .date {
+//   letter-spacing: 0.1em;
+//   font-size: 24px;
+// }
+// .text {
+//   letter-spacing: 0.1em;
+//   font-size: 12px;
+//   padding: 20px 0 0;
+// }
+
+const Clock = () => {
+  return (
+    <div
+      css={css`
+        width: 100%;
+        font-family: 'Share Tech Mono', monospace;
+        color: #ffffff;
+        text-align: center;
+
+        color: #daf6ff;
+        text-shadow: 0 0 ${rem(24)} rgba(10, 175, 230, 1),
+          0 0 ${rem(24)} rgba(10, 175, 230, 0);
+      `}
+    >
+      <p
+        css={css`
+          letter-spacing: 0.1em;
+          font-size: ${rem(14)};
+
+          margin-bottom: ${rem(12)};
+        `}
+      >
+        ZAI Version 2
+      </p>
+
+      <Countdown
+        date={1611007200 * 1000}
+        renderer={({ days, hours, minutes, seconds }) => {
+          return (
+            <p
+              css={css`
+                font-family: 'Share Tech Mono', monospace;
+                color: #ffffff;
+                text-align: center;
+
+                font-weight: 100;
+
+                letter-spacing: 0em;
+                font-size: ${rem(62)};
+                padding: ${rem(5)} 0;
+
+                width: 100%;
+                display: flex;
+                justify-content: space-around;
+                color: #fff;
+
+                margin-bottom: ${rem(34)};
+              `}
+            >
+              <span>{days}D</span>:<span>{hours}H</span>:<span>{minutes}M</span>
+              :<span>{seconds}S</span>
+            </p>
+          )
+        }}
+      />
+
+      <p
+        css={css`
+          letter-spacing: 0.1em;
+          font-size: ${rem(24)};
+
+          margin-bottom: ${rem(42)};
+        `}
+      >
+        Fair Launch - No Premine
+      </p>
+      <p
+        css={css`
+          margin-bottom: ${rem(12)};
+        `}
+      >
+        ZAI:DAI peg
+      </p>
+      <p
+        css={css`
+          margin-bottom: ${rem(12)};
+        `}
+      >
+        5 day initial bootstrap
+      </p>
+    </div>
+  )
+}
+
 const Overlay = () => {
   return (
     <OverlayContainer>
       <ModalBox
         css={css`
           width: 100%;
-          max-width: ${rem(520)};
+          max-width: ${rem(640)};
 
           overflow: hidden;
+
+          box-shadow: 0 ${rem(18)} ${rem(48)} rgba(0, 0, 0, 0.2);
         `}
       >
         <ColorRush>
@@ -77,62 +178,31 @@ const Overlay = () => {
 
                 align-items: center;
 
-                padding: ${rem(24)} ${rem(24)} ${rem(24)};
+                padding: ${rem(40)} ${rem(32)} ${rem(32)};
+
+                background: #0f3854;
+                background: radial-gradient(
+                  ellipse at center,
+                  #0a2e38 0%,
+                  #000000 70%
+                );
+                background-size: 100%;
+
+                color: #fff;
               `}
             >
-              <h4
+              <Clock></Clock>
+
+              <p
                 css={css`
-                  margin-bottom: ${rem(60)};
+                  opacity: 0.92;
+                  font-size: ${rem(12)};
+
+                  margin-top: ${rem(64)};
                 `}
               >
-                Version 2 is coming
-              </h4>
-
-              <Countdown
-                date={1611007200 * 1000}
-                renderer={({ days, hours, minutes, seconds }) => {
-                  return (
-                    <h3
-                      css={css`
-                        width: 100%;
-                        display: flex;
-                        justify-content: space-around;
-
-                        font-size: ${rem(52)};
-
-                        margin-bottom: ${rem(60)};
-                      `}
-                    >
-                      <span>{days}D</span>
-                      <span>{hours}H</span>
-                      <span>{minutes}M</span>
-                      <span>{seconds}S</span>
-                    </h3>
-                  )
-                }}
-              />
-
-              <h2
-                css={css`
-                  margin-bottom: ${rem(16)};
-                  line-height: 1.2;
-                  font-size: ${rem(24)};
-                `}
-              >
-                100:1 Swap
-              </h2>
-
-              <h2
-                css={css`
-                  margin-bottom: ${rem(48)};
-                  line-height: 1.2;
-                  font-size: ${rem(24)};
-                `}
-              >
-                5 Day Bootstrap
-              </h2>
-
-              <p>Swap available soon</p>
+                Permissionless swap of ZAI v1 for ZAI v2 (100:1) available soon
+              </p>
             </div>
           </div>
         </ColorRush>
