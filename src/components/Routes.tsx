b9f51eb3aa452b39e72e7d2f26da291b355ea1d6
redacted007 <77245032+redacted007@users.noreply.github.com>
2021-01-18 19:06:41 -0500

    Update Routes.tsx

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Governance from './Governance'
 
@@ -117,8 +112,6 @@ const MainRoutes = () => {
             <Route path="/connect">
               <ConnectModal />
             </Route>
-
-            <Redirect to="/welcome" />
           </Switch>
         </Route>
       </Switch>
